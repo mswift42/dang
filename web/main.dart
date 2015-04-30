fd79acbf6991dec5b805e4361091615a0fc2293c
martin.haesler <martin.haesler@gmail.com>
2015-04-30 21:10:02 +0100

    Finish 5 min Quickstart.

--- web/main.dart
+++ web/main.dart
@@ -1,8 +1,25 @@
 // Copyright (c) 2015, <your name>. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
-import 'dart:html';
+import 'package:angular2/angular2.dart';
+
+// These imports will go away soon:
+import 'package:angular2/src/reflection/reflection.dart' show reflector;
+import 'package:angular2/src/reflection/reflection_capabilities.dart' show ReflectionCapabilities;
+
+@Component(
+selector: 'my-app'
+)
+
+@View(
+template: '<h1>Hello {{ name }}</h1>'
+)
+
+class AppComponent{
+  String name = 'martin';
+}
 
 void main() {
-  querySelector('#output').text = 'Your Dart app is running.';
+  reflector.reflectionCapabilities = new ReflectionCapabilities();
+  bootstrap(AppComponent);
 }
