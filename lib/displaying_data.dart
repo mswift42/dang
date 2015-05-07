61aa1241ff39ad0e0b67dba24881b6cb29a5d6ca
martin.haesler <martin.haesler@gmail.com>
2015-05-07 19:54:47 +0100

    Remove reflection imports

--- lib/displaying_data.dart
+++ lib/displaying_data.dart
@@ -1,9 +1,7 @@
 library displaying_data;
 
 import 'package:angular2/angular2.dart';
-import 'package:angular2/src/reflection/reflection.dart' show reflector;
-import 'package:angular2/src/reflection/reflection_capabilities.dart'
-    show ReflectionCapabilities;
+
 
 @Component(selector: 'display')
   
