88ed7ea4f64aceedd20b38ca7111ce17cbbcacdc
martin.haesler <martin.haesler@gmail.com>
2015-05-07 20:00:57 +0100

    Import displaying_data, bootstrap it

--- web/main.dart
+++ web/main.dart
@@ -2,6 +2,7 @@
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 import 'package:angular2/angular2.dart';
+import 'package:dang/displaying_data.dart';
 
 // These imports will go away soon:
 import 'package:angular2/src/reflection/reflection.dart' show reflector;
@@ -19,4 +20,5 @@ class AppComponent {
 void main() {
   reflector.reflectionCapabilities = new ReflectionCapabilities();
   bootstrap(AppComponent);
+  bootstrap(DisplayComponent);
 }
