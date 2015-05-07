5111d518ce392d6c3cf5f2a769fc7214f690620a
martin.haesler <martin.haesler@gmail.com>
2015-05-07 20:20:29 +0100

    Add DisplayFriends class

--- lib/display_friends.dart
+++ lib/display_friends.dart
@@ -1,1 +1,16 @@
 library display_friends;
+
+import 'package:angular2/angular2.dart';
+
+@Component(selector: 'display-friends')
+
+@View(
+    templateUrl: '../views/displayfriends.html',
+    directives: const[For]
+      )
+
+
+  class DisplayFriends {
+    String myName: = 'Martin';
+    List<String> friendNames = ['Aarav', 'Martín', 'Shannon', 'Ariana', 'Kai'];
+  }
