6625fa21ad85d926dda56f4d906b286af384bd08
martin.haesler <martin.haesler@gmail.com>
2015-05-12 19:38:28 +0100

    Declare @View and @Component.

--- lib/todo_list.dart
+++ lib/todo_list.dart
@@ -1,6 +1,10 @@
 library todo_list;
 
+import 'package:angular2/angular2.dart';
 
+@Component(selector: 'todo-list')
+
+@View(templateUrl: '../viewr/todoinput.html')
 
 class TodoList {
   List<String> todos =
