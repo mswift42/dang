d0e966437e763911312a6432a7d03266400edb2e
martin.haesler <martin.haesler@gmail.com>
2015-05-12 19:33:55 +0100

    Add TodoList Controller

--- lib/todo_list.dart
+++ lib/todo_list.dart
@@ -1,1 +1,12 @@
 library todo_list;
+
+
+
+class TodoList {
+  List<String> todos =
+    ['Eat breakfast', 'Walk dog', 'Breathe', 'Learn Angular'];
+    
+  addTodo(String todo) {
+    todos.add(todo);
+  }
+}
