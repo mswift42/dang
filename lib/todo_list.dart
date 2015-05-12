15d94d4b3e744166163c95deea451d6cb879b86e
martin.haesler <martin.haesler@gmail.com>
2015-05-12 19:49:04 +0100

    Format using dartfmt.

--- lib/todo_list.dart
+++ lib/todo_list.dart
@@ -1,20 +1,22 @@
 library todo_list;
 
 import 'package:angular2/angular2.dart';
+import 'dart:html';
 
 @Component(selector: 'todo-list')
-
-  @View(templateUrl: '../viewr/todoinput.html',
-      directives: const [For])
-
+@View(templateUrl: '../viewr/todoinput.html', directives: const [For])
 class TodoList {
-  List<String> todos =
-    ['Eat breakfast', 'Walk dog', 'Breathe', 'Learn Angular'];
-    
+  List<String> todos = [
+    'Eat breakfast',
+    'Walk dog',
+    'Breathe',
+    'Learn Angular'
+  ];
+
   addTodo(String todo) {
     todos.add(todo);
   }
-   doneTyping(KeyboardEvent event) {
+  doneTyping(KeyboardEvent event) {
     if (event.keyCode == KeyCode.ENTER) {
       InputElement e = event.target;
       addTodo(e.value);
